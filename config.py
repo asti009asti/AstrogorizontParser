b52fab014762eb5a8eb637c56359b96cf15cd545
asti009asti <smilev@gmail.com>
2014-10-03 11:25:01 +0100

    config file updated with google controls

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ AG_DATES_XPATH = "//td[@bgcolor='#474E63'][span[@class='datarticles']]"  # story
 AG_DESCIPTION_XPATH = "//td[@colspan='2'][@valign='top']"  # story descriptions
 AG_NEXTPAGES = "//table[14]/tbody/tr/td/span[@class='gorlin']/a[position()=1]"  # next 10 pages links
 AG_PERIOD = 10  #time period to export stories for
+
+G_SEARCH = "gbqfq"
+G_NEXTPAGE = "//a[@id='pnnext']/span[2]"
+G_DOMAINS = "//cite[@class='_Rm' or @class='_Rm bc']"
